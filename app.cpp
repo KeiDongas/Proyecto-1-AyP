a19f3900c3e35e6447f5f0f47feb9a7a4fd5e195
Saul <saulbetancourt08@gmail.com>
2026-05-25 19:11:37 -0400

    añadir variables de puntaje

--- app.cpp
+++ app.cpp
@@ -9,6 +9,11 @@ int main(){
     int palos; // Palos de cartas
     int comodin1, comodin2, comodin3; // Comodines
     int blind; // Blind (Ciega)
+    int points; // Puntaje
+    int mult; // Multiplicadores
+    int fichas; // Total de fichas de la mano
+
+    // Formula: (Valor de la mano + Total de puntos de cada carta) * Mult
 
     // Pedir datos al usuario
     cin >> card1 >> card2 >> card3 >> card4 >> card5; // Pedir mano de cartas
